60fb85bc4cb92e96d7ef2f8acfe54e5b213f2ecb
Mogeko-Kruger <75345791+Mogeko-Kruger@users.noreply.github.com>
2022-01-10 06:33:41 -0300

    Add files via upload

--- Boxes/appdata.js
+++ Boxes/appdata.js
@@ -1,56 +1,119 @@
-class ListBinding {
-    constructor(element) {
-        this.ulElement = element;
+//setting the todo list, this array will get the task inserted by the user and be assigned
+//as the text (string), the checked (bool) and the id, which is date.now() to assign a
+//unique number
+let todoItems = [];
 
-        this.textArray = [];
+function renderTodo (todo) {
+    localStorage.setItem('todoItemsRef', JSON.stringify(todoItems));
+
+    const list = document.querySelector('.js-todo-list');
+
+    // I don't know what these brackets are doing
+    const item = document.querySelector(`[data-key='${todo.id}']`);
+
+    if (todo.deleted) {
+        item.remove();
+        return
     }
+
+    const isChecked = todo.checked ? 'done' : '';
+
+    const node = document.createElement('li');
+
+    node.setAttribute('class', `todo-item ${isChecked}`);
+
+    node.setAttribute('data-key', todo.id);
+
+    node.innerHTML = `
+    <input id='${todo.id}' type='checkbox'/>
+    <label for='${todo.id}' class='tick js-tick'></label>
     
-    static createList (text) {
-        const li = document.createElement('li');
+    <span>${todo.text}</span>
+    <button class='delete-todo js-delete-todo'>
+    <i class="fas fa-times"></i>
+    </button>
+    `;
 
-        li.innerHTML = text;
+    if (item) {
+        list.replaceChild(node, item);
+    } else {
 
-        return li;
-    }
+        list.append(node);
 
-    update () {
-        while(this.ulElement.firstChild) {
-            this.ulElement.removeChild(this.ulElement.firstChild);
-        }
-
-        for (const i of this.textArray) {
-            this.ulElement.appendChild(ListBinding.createList(i));                        
-        }
-    }
-
-    add (text) {
-        this.textArray.push(text);
-        this.update();
-    }
-
-    remove (index) {
-        this.textArray.splice(index, 1);
-        this.update();
     }
 }
 
-const myList = document.querySelector('.myList');
-const listBinding = new ListBinding(myList);
+function addTodo (text) {
+    const Todo = {
+        text,
+        checked: false,
+        id: Date.now(),
+    };
 
-function addToList () {
-    const userInput = document.getElementById('userInput').value; 
+    todoItems.push(Todo);
+    renderTodo(Todo);
+}
 
-    listBinding.add(userInput);
+function toggleDone(key) {
+  // findIndex is an array method that returns the position of an element
+  // in the array.
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  // Locate the todo item in the todoItems array and set its checked
+  // property to the opposite. That means, `true` will become `false` and vice
+  // versa.
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
+}  
 
+function deleteTodo(key) {
+    const index = todoItems.findIndex(item => item.id === Number(key));
+
+    const Todo = {
+        deleted: true,
+        ...todoItems[index]
+    };
+
+    todoItems = todoItems.filter(item => item.id !== Number(key));
+    renderTodo(Todo);
+}
+
+const form = document.querySelector(".js-form");
+
+form.addEventListener('submit', event => {
+
+    event.preventDefault();
+
+    const input = document.querySelector('.js-todo-input');
     
-}
+    const text = input.value.trim();
 
-function listRemoval () {
-    const listRemover = document.getElementById('listRemover').value;
+    if (text !== '') {
+        addTodo(text);
+        input.value = '';
+        input.focus();
+    }
+});
 
-    listBinding.remove(listRemover);
-}
+const list = document.querySelector('.js-todo-list');
 
-function buttonClicked () {
-    listBinding.add('Hello!');
-}
+list.addEventListener('click', event => {
+    if (event.target.classList.contains('js-tick')) {
+        const itemKey = event.target.parentElement.dataset.key;
+        toggleDone(itemKey);
+    }
+
+    if (event.target.classList.contains('js-delete-todo')) {
+        const itemKey = event.target.parentElement.dataset.key;
+        deleteTodo(itemKey);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const ref = localStorage.getItem('todoItemsRef');
+    if (ref) {
+        todoItems = JSON.parse(ref);
+        todoItems.forEach(t => {
+            renderTodo(t);
+        });
+    }
+});
